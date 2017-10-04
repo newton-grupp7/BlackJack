76a95eca525149d196f1a0a949b1c21abf481a22
jonkeponke <jjonaas1@hotmail.com>
2017-10-04 11:35:33 +0200

    Second version

--- BlackJack/src/Game.java
+++ BlackJack/src/Game.java
@@ -1,20 +1,52 @@
+import java.util.Scanner;
 
 public class Game {
 	
-	static CardDeck skapaKortlek = new CardDeck();
+	public static CardDeck cardDeckObject = new CardDeck(); // Skapar ett objekt av klassen CardDeck
 	
 	public static void main(String[] args) {
-		Game game = new Game();
 		
-		System.out.println("Välkommen till BlackJack");
-		skapaKortlek.createCardDeck();
-		System.out.println(game.getCards());
+		Game player = new Game(); //Skapar ett objekt av klassen Game
+		cardDeckObject.createCardDeck(); // Skapar kortleken
+		int sum;
 		
+		System.out.println("Welcome to BlackJack");
+
+		int card1 = player.getCard(); //Får första kortet
+		int card2 = player.getCard(); //Får andra kortet
+		sum = card1 + card2;
+		
+		System.out.println("You get a " + card1 + " and a " + card2);
+		System.out.println("Your total is " + sum);
+		
+		Scanner scan = new Scanner(System.in);
+		int proceed = scan.nextInt();
+		
+		boolean run = false;
+		if (proceed == 0)
+			run = true;
+		
+		while (run == false) {
+			
+			if (proceed == 1) {
+				int newCard = player.getCard();
+				sum += newCard;
+				System.out.println("You drew a " + newCard);
+				System.out.println("Your total is " + sum);
+				if (sum > 21) {
+					System.out.println("You lost");
+					break;
+				}
+				if (sum == 21)
+					run = true;
+			}
+		}
+		scan.close();
 	}
 	
-	private int getCards() {
-		int kort = skapaKortlek.cardDeck.get(0);
-		skapaKortlek.cardDeck.remove(0);
+	public int getCard() { //Metoden för att få kort
+		int kort = cardDeckObject.cardDeck.get(0); //Ger kort värdet av kortet på index 0
+		cardDeckObject.cardDeck.remove(0); //Tar bort det kortet ifrån kortleken
 		return kort;
 	}
 	
