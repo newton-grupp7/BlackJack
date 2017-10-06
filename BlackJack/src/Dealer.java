3b0e6739fe34b92c6529c68a70e3300e0127dc6e
jonkeponke <jjonaas1@hotmail.com>
2017-10-06 14:26:01 +0200

    6th Version

--- BlackJack/src/Dealer.java
+++ BlackJack/src/Dealer.java
@@ -1,30 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
-public class Dealer extends CardDeck {
+public class Dealer {
 
-	public int playerSum;
-	public int dealerSum;
-	public int dealerSecondCard;
-	public String hitOrStay = "'h' for hit, 's' for stay";
-
-	public ArrayList<Integer> createCardDeck() {
-
-		for (int i = 0; i < 4; i++)
-			cardDeck.add(11);
-		for (int j = 0; j < 16; j++)
-			cardDeck.add(10);
-		for (int k = 2; k <= 9; k++)
-			for (int i = 0; i < 4; i++)
-				cardDeck.add(k);
-
-		Collections.shuffle(cardDeck);
-		return cardDeck;
-
+	private int playerSum;
+	private int dealerSum;
+	private int dealerSecondCard;
+	private String hitOrStay = "'h' for hit, 's' for stay";
+	CardDeck cardDeck; // = new CardDeck();
+	
+	public Dealer() {
+		super();
+		cardDeck = new CardDeck();
 	}
 
-	public void initiateGame() {
+	void initiateGame() {
 		int firstCard = giveCard(); // Dealern ger första kortet
 		int secondCard = giveCard(); // Dealern ger andra kortet
 		playerSum = firstCard + secondCard;
@@ -38,68 +27,82 @@ public class Dealer extends CardDeck {
 		// Ger två kort till dealern men skriver bara ut det första kortet
 	}
 
-	public int giveCard() { // Metoden för att ge kort
-		int kort = cardDeck.get(0); // Ger kort värdet av kortet på index 0
-		cardDeck.remove(0); // Tar bort det kortet ifrån kortleken
+	int giveCard() { // Metoden för att ge kort
+		int kort = cardDeck.getCards().get(0); //cardDeck.get(0); // Ger kort värdet av kortet på index 0
+		cardDeck.getCards().remove(0);
 		return kort;
 	}
 
-	public void proceedGame () {
+	void proceedGame () {
 		int newCard;
 		newCard = giveCard();
 		playerSum += newCard;
 		System.out.println("You drew a " + newCard + "\nYour total is " + playerSum);
-	
 	}
 
-	public void loopGame(String proceed, Scanner scan) { //Metoden för att loopa spelet tills man vinner, väljer att stanna eller förlorar
-		boolean run = true;
+	boolean playerStay(String proceed, Scanner scan) { //Metoden för att loopa spelet tills man vinner, väljer att stanna eller förlorar
 		
-		while (run == true) {
-			
-			if (proceed.equals("s")) {
-				run = false;
-				break;
-			}
-			if (proceed.equals("h")) {
-				
+//		if (!(proceed.equals("s")) || !(proceed.equals("h"))) { // "Felmeddelande" om man inte skriver in 's' eller 'h'
+//			
+//			while (!(proceed.equals("s")) || !(proceed.equals("h"))) {
+//				System.out.println(hitOrStay + " i else if");
+//				proceed = scan.next();
+//				if (proceed.equals("s") || proceed.equals("h"))
+//					break;
+//			}
+//		}
+		
+		if (proceed.equals("s")) {
+			return true;
+		}
+		
+		if (proceed.equals("h")) {
+			while (proceed.equals("h")) {
+
 				if (playerSum < 21) { //Om man får under 21 och väljer att 'hit' så fortsätter spelet
 					proceedGame();
 				}
 				
 				if (playerSum > 21) { // Om man får över 21
-					System.out.println("\tYou lost");
+					System.out.println("You lost");
 					break;
 				}
 
 				if (playerSum == 21) { // Om man får 21
 					System.out.println("Blackjack!");
-					run = false;
 					break;
 				}
 				System.out.println("\n'h' for hit, 's' for stay");
-				proceed = scan.nextLine();
+				proceed = scan.next();
 			}
-			else if (!(proceed.equals("s")) || !(proceed.equals("h"))) { //"Felmeddelande" om man inte skriver in 's' eller 'h'
-				System.out.println(hitOrStay);
-				proceed = scan.nextLine();
-			}
-		}
-	}
-	public boolean dealerWon () {
-		System.out.println("Dealer's turn\nHis hidden card was a " + dealerSecondCard + "\nHis total was " + dealerSum);
-		while (dealerSum < 17) { // Dealern måste ta "hit" när den har mindre än 17
-			int newCard = giveCard();
-			dealerSum = dealerSum + newCard;
-			System.out.println("\nDealer chooses to hit\nHe draws a " + newCard + "\nHis total is " + dealerSum);
-		}
-		if (playerSum > dealerSum || dealerSum > 21) { // Spelaren vinner om playerSum är över dealerSum eller dealer får över 21
-			System.out.println("\nYou win");
 			return false;
 		}
-		else {
-			System.out.println("\nDealer win");
+		return false;
+
+	}
+		
+	boolean dealerWon () {
+		if (playerSum > 21) 
 			return true;
-		}		
+		
+		if(!(playerSum > 21)) {
+			System.out.println("\nDealer's turn\nHis hidden card was a " + dealerSecondCard + "\nHis total was " + dealerSum);
+			
+			while (dealerSum < 17) { // Dealern måste ta "hit" när den har mindre än 17
+				int newCard = giveCard();
+				dealerSum = dealerSum + newCard;
+				System.out.println("\nDealer chooses to hit\nHe draws a " + newCard + "\nHis total is " + dealerSum+ "\nYour total was " + playerSum);	
+			}
+			
+			if (playerSum > dealerSum && playerSum <= 21|| dealerSum > 21) { // Spelaren vinner om playerSum är över dealerSum eller dealer får över 21
+				System.out.println("\nYou win");
+				return false;
+			}
+			else {
+				System.out.println("\nDealer win");
+				return true;
+			}
+		}
+		return true;
 	}
 }
--- BlackJack/src/Game.java
+++ BlackJack/src/Game.java
@@ -2,46 +2,92 @@ import java.util.Scanner;
 
 public class Game {
 
-	public static Dealer dealer = new Dealer(); // Skapar ett objekt av klassen Dealer
-
 	public static void main(String[] args) {
+		
+		Dealer dealer = new Dealer(); // Skapar ett objekt av klassen Dealer
+		Scanner scan = new Scanner(System.in);
 
-		Scanner scanMoney = new Scanner(System.in);
+		boolean playAgain = true;
+
 		System.out.println("Welcome to BlackJack!\n\nHow much money do you have?");
-		int balance = scanMoney.nextInt();
+		int balance = scan.nextInt();
 
 		while (balance == 0) {
 			System.out.println("You can't play without any money\nHow much money do you have?");
-			balance = scanMoney.nextInt();
-		}
-		System.out.println("How much do you want to bet?");
-		int wager = scanMoney.nextInt();
-
-		while (wager == 0) {
-			System.out.println("You have to bet money to play\nHow much do you want to bet?");
-			wager = scanMoney.nextInt();
-		}
-
-		balance = balance - wager;
-
-		System.out.println("Your balance is " + balance + ", " + wager + " is on the table");
-		dealer.createCardDeck(); // Skapar kortleken och blandar den
-		dealer.initiateGame(); // Startar spelet och delar ut två kort till spelaren
-
-		Scanner scanHitOrStay = new Scanner(System.in);
-		String proceed = scanHitOrStay.nextLine();
-
-		dealer.loopGame(proceed, scanHitOrStay); // Loopar spelet tills man vinner, väljer att stanna eller förlorar
-		boolean lost = dealer.dealerWon();
-		
-		if (!lost) {
-			balance = balance + ( wager*2) ;
-			System.out.println("Your balance is " + balance);
+			balance = scan.nextInt();
 		}
 		
-		scanHitOrStay.close();
-		scanMoney.close();
+		while (playAgain) { // Så länge som playAgain är true så körs spelet
+			
+			System.out.println("How much do you want to bet?");
+			int wager = scan.nextInt();
+			
+			while (wager == 0) {
+				System.out.println("You have to bet money to play\nHow much do you want to bet?");
+				wager = scan.nextInt();
+			}
+			while (wager > balance) {
+				System.out.println("You can't bet more money than you have\nHow much do you want to bet?");
+				wager = scan.nextInt();
+			}
+	
+			balance = balance - wager;
+	
+			System.out.println("Your balance is " + balance + ", " + wager + " is on the table");
+			dealer.cardDeck.createCardDeck(); // Skapar kortleken och blandar den
+			dealer.initiateGame(); // Startar spelet och delar ut två kort till spelaren
+	
+			String proceed = scan.next();
 
+			dealer.playerStay(proceed, scan); // 
+			boolean dealerWin = dealer.dealerWon();
+
+			String yesOrNo;
+			
+			if (balance > 0) { // Om man har pengar kvar så ska man få alternativet att spela igen
+				if (!dealerWin)
+					balance = balance + ( wager * 2 );
+				if (dealerWin) {
+					while (balance > 0) {
+
+						System.out.println("Your balance is " + balance + "\n\nWould you like to play again? 'y' for yes, 'n' for no");
+						yesOrNo = scan.next();
+						if (yesOrNo.equals("y")) {
+							playAgain = true;
+							break;
+						}
+						else if (yesOrNo.equals("n")) {
+							playAgain = false;
+							break;
+						}
+						else
+							System.out.println("'y' for yes, 'n' for no");
+					}
+				}
+				else {
+					while (balance > 0) {
+
+						System.out.println("Your balance is " + balance + "\n\nWould you like to play again? 'y' for yes, 'n' for no");
+						yesOrNo = scan.next();
+						if (yesOrNo.equals("y")) {
+							playAgain = true;
+							break;
+						}
+						else if (yesOrNo.equals("n")) {
+							playAgain = false;
+							break;
+						}
+						else
+							System.out.println("'y' for yes, 'n' for no");
+					}
+				}
+			}
+			else  {
+				System.out.println("Your balance is " + balance +  "\nYou're out of money");
+				playAgain = false;
+			}
+		}
+		System.out.println("Thank you for playing!");
+		scan.close();
 	}
-
 }
