4e22fb914f81417a8d45c087b2d45014b9852b7b
keena <keenafaison@gmail.com>
2017-10-12 14:59:15 +0200

    Uppdaterade versionen av uppercase letters

--- BlackJack/src/Dealer.java
+++ BlackJack/src/Dealer.java
@@ -52,7 +52,7 @@ public class Dealer {
 		}
 
 		else if (proceed.equals("h") || proceed.equals("H")) {
-			while (proceed.equals("h")) {
+			while (proceed.equals("h") || proceed.equals("H")) {
 
 				if (playerSum < 21) { // Om man f�r under 21 och v�ljer att 'hit' s� forts�tter spelet
 					proceedGame();
